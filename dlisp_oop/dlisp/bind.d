21da6a43877c5b86e31aa7b3deadb54924580c1f
klaus.blindert <klaus.blindert@web.de>
2008-03-14 18:30:36 +0000

    Automatic binding generator.

--- dlisp_oop/dlisp/bind.d
+++ dlisp_oop/dlisp/bind.d
@@ -32,18 +32,124 @@ public {
   import std.traits : ReturnType, ParameterTypeTuple;
 }
 
-PredefFunc toDelegate2(PredefFuncPtr fn)
-{ 
-    struct holder {
-        PredefFuncPtr f;
-        Cell* call(DLisp dlisp, Cell* cell)
-        {
-            return f(dlisp,cell);
-        }
+template FunParams(alias f)
+{
+  const FunParams = FunParamsImpl!(0, ParameterTypeTuple!(f));
+}
+
+template FunParamsImpl(int n, A ... )
+{
+    static if( A.length == 0 ) {
+      const FunParamsImpl = "";
+    } else {
+      static if( A.length > 1 ) {
+        const FunParamsImpl = FunParamsImpl!(n,A[0]) ~ "," ~ FunParamsImpl!(n+1,A[1..$]);
+      } else {
+        const FunParamsImpl = "unbox!(" ~ A[0].stringof ~ ")(args[" ~ n.stringof ~"])";
+      }
     }
-    auto res = new holder;
-    res.f = fn;
-    return &res.call;
+}
+
+template HasParams(alias f)
+{
+    const HasParams = ParameterTypeTuple!(f).length > 0;
+}
+
+template MethodName(alias f)
+{
+  const MethodName = (&f).stringof[1..$];
+}
+
+template InvokeMethod(alias func)
+{
+  static if( !HasParams!(func) ) {
+    const InvokeMethod = BindReturnValue!(func) ~ "instance." ~ func.stringof ~ ";";
+  } else {
+    const InvokeMethod = BindReturnValue!(func) ~ "instance." ~ MethodName!(func) ~ "("~ FunParams!(func) ~");";
+  }
+}
+
+template HasReturnType(alias func)
+{
+  const HasReturnType = !is(ReturnType!(func) == void);
+}
+
+template BindReturnValue(alias func)
+{
+  static if( !HasReturnType!(func) )
+    const BindReturnValue = "";
+  else
+    const BindReturnValue = "auto return_value = ";
+}
+
+template BoxArguments(T ...)
+{
+  static if( T.length == 0)
+    const BoxArguments = "";
+  else static if( T.length == 1 )
+    const BoxArguments = "nextarg;" ~ BoxArgument!(T[0]);
+  else
+    const BoxArguments = "nextarg;" ~ BoxArgument!(T[0]) ~ BoxArguments!(T[1..$]);
+}
+
+template AddArgument(string argvalue)
+{ 
+    const AddArgument ="args~=" ~ argvalue ~ ";";
+}
+
+template CheckArgument(string checkfun)
+{
+    const CheckArgument = 
+      "if( !" ~ checkfun ~ "(arg_car) ) { "
+        ~ "throw new ArgumentState(\"Excepted " ~ checkfun ~ "\",cell.pos);"
+        ~ "}";
+}
+
+template BoxArgument(T : int)
+{
+  const BoxArgument = CheckArgument!("isInt") ~ AddArgument!("box(arg_car.intValue)");
+}
+
+template BoxArgument(T : long)
+{
+  const BoxArgument = CheckArgument!("isInt") ~ AddArgument!("box(arg_car.intValue)");
+}
+
+template BoxArgument(T : string)
+{
+  const BoxArgument = CheckArgument!("isString") ~ AddArgument!("box(arg_car.strValue)");
+}
+
+template BoxArgument(T)
+{
+  pragma(msg,"DON'T KNOW HOW TO AUTOMATICALLY BIND THIS: " ~ T.stringof);
+  static assert(0);
+}
+
+template BoxReturnValue(T : int)
+{
+  const BoxReturnValue = "return newInt(return_value);";
+}
+
+template BoxReturnValue(T : long)
+{
+  const BoxReturnValue = "return newInt(return_value);";
+}
+
+template BoxReturnValue(T : bool)
+{
+  const BoxReturnValue = "return newBool(return_value);";
+}
+
+template BoxReturnValue(T : string)
+{
+  const BoxReturnValue = "return newStr(return_value);";
+}
+
+template BoxReturnValue(T)
+{
+  pragma(msg,"DON'T KNOW HOW TO AUTOMATICALLY WRAP RETURN TYPE: " ~ T.stringof);
+  static assert(0);
 }
 
 template BoundClass(string classname)
@@ -82,7 +188,7 @@ template BoundClass(string classname)
     if( _classCell is null )
     {
       _classCell = newObject(classname);
-      _classCell.table["MAKE-INSTANCE"] = newPredef("MAKE-INSTANCE",toDelegate2(&makeInstance),"CREATE AN INSTANCE OF " ~ toupper(classname));
+      _classCell.table["MAKE-INSTANCE"] = newPredef("MAKE-INSTANCE",toDelegate(&makeInstance),"CREATE AN INSTANCE OF " ~ toupper(classname));
       foreach(string name, Cell* method; _methods)
       {
         _classCell.table[name] = method;
@@ -92,20 +198,65 @@ template BoundClass(string classname)
   }
 }
 
-
 template BoundMethod(string name,alias func)
 {
+//     pragma(msg,name);
     static this()
     {
-      static Cell* implwrapper(DLisp dlisp, Cell* cell)
+      static Cell* methodWrapper(DLisp dlisp, Cell* cell)
       {
         Cell* objectCell = cell.cdr.car;
         auto instance = getInstance(dlisp.eval(objectCell));
 
-        writefln ("METHOD: %s SELF: %s ARGS: %s",name, instance, cellToString(objectCell.cdr));
+//         writefln ("METHOD: %s SELF: %s ARGS: %s",name, instance, cellToString(cell.cdr.cdr));
+
+        if(ParameterTypeTuple!(func).length > listLen(cell.cdr.cdr))
+        {
+          throw new ArgumentState("Function " ~ name ~ " got too few arguments.",cell.pos);
+        }
+
+        if(ParameterTypeTuple!(func).length < listLen(cell.cdr.cdr))
+        {
+          throw new ArgumentState("Function " ~ name ~ " got too many arguments.",cell.pos);
+        }
+
+        Cell* arg_cons = cell.cdr.cdr;
+        Cell* arg_car = null;
+
+        void nextarg()
+        {
+          assert(arg_cons);
+          arg_car = dlisp.eval(arg_cons.car);
+          arg_cons = arg_cons.cdr;
+        }
+
+        Box[] args;
+        mixin(BoxArguments!(ParameterTypeTuple!(func)));
+        mixin(InvokeMethod!(func));
+
+        static if( HasReturnType!(func) ){
+          mixin(BoxReturnValue!(ReturnType!(func)));
+        }
         return null;
       }
 
-      _methods[name] = newPredef(name,toDelegate2(&implwrapper),"Autogenerated unbound function.");
+      _methods[name] = newPredef(name,toDelegate(&methodWrapper),"Autogenerated unbound function.");
     }
+}
+
+string _lispify(string name)
+{
+  string new_name;
+  for(int i= 1; i != name.length; ++i)
+  {
+    if( toupper(name[i..i+1]) == name[i..i+1] )
+      new_name ~= '-';
+    new_name ~= name[i];
+  }
+  return toupper(new_name);
+}
+
+template BoundMethod(alias func)
+{
+  mixin BoundMethod!(_lispify(MethodName!(func)),func);
 }
