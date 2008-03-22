9e0c55266fb39af6e861326aa1739a1e10fd9975
klaus.blindert <klaus.blindert@web.de>
2008-03-22 09:21:28 +0000

    Added Character GObject type.

--- src/interfaces.d
+++ src/interfaces.d
@@ -62,7 +62,7 @@ string typeToString(ObjectType t)
 
 interface ICharacter
 {
-    dstring getName();
+//     dstring getName();
     int getAttribute(Attribute a);
 
     // PRIMARY ATTRIBUTES
@@ -76,22 +76,66 @@ interface ICharacter
     float getWeight();
 
     int getMaxHP();
-    int getCurrentHP();
+    int getHP();
 
     int getMaxEnergy();
-    int getCurrentEnergy();
+    int getEnergy();
 
-    int getArmorEff();
-    int getShieldEff();
-
-    // STATE VALUES
-    bool isShieldOn();
-    bool isShaded();
-
-    // CAPABILITIES
-    bool canEquip(IItem item);
+//     int getArmorEff();
+//     int getShieldEff();
+// 
+//     // STATE VALUES
+//     bool isShieldOn();
+//     bool isShaded();
+// 
+//     // CAPABILITIES
+//     bool canEquip(IItem item);
 }
 
 interface IItem
 {
+}
+
+
+template AttributedObject()
+{
+    private:
+        Attribute[] _attributes;
+        float _weight;
+
+        int _max_hp;
+        int _hp;
+
+        int _max_energy;
+        int _energy;
+
+    public:
+
+        int getAttribute(Attribute a)
+        {
+            return _attributes[a];
+        }
+
+        int getSTR() {
+            return getAttribute(Attribute.STR);
+        }
+        int getDEX() {
+            return getAttribute(Attribute.DEX);
+        }
+        int getEND() {
+            return getAttribute(Attribute.END);
+        }
+        int getINT() {
+            return getAttribute(Attribute.INT);
+        }
+        int getPER() {
+            return getAttribute(Attribute.PER);
+        }
+
+        float getWeight() { return _weight; }
+
+        int getMaxHP() { return _max_hp; }
+        int getHP() { return _hp; }
+        int getMaxEnergy() { return _max_energy; }
+        int getEnergy() { return _energy; }
 }
