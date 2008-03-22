6be86a172b593010d78b0f47ae1be6644a438e6e
klaus.blindert <klaus.blindert@web.de>
2008-03-22 12:39:16 +0000

    Some rework of tile objects.

--- src/dataset.d
+++ src/dataset.d
@@ -13,133 +13,14 @@ private {
 
     import vector;
 
-    import gobject;
+    import modelmesh;
 
     import dlisp.bind;
 }
 
-class FaceInfo
-{
-    public:
-        float[][] data;
-
-    this(sofu.List list)
-    {
-        foreach(sofu.SofuObject f; list)
-        {
-            float[] face;
-            for(int i=0; i!=8;++i)
-            {
-                face ~= f.asList().value(i).toFloat();
-            }
-            data ~= face;
-        }
-    }
-
-    void draw(float dx, float dy, float dz)
-    {
-        for(int i=0; i!= data.length; ++i)
-        {
-            glTexCoord2f(data[i][3],data[i][4]);
-            glNormal3f(data[i][5],data[i][6],data[i][7]);
-            glVertex3f(data[i][0]+dx,data[i][1]+dy,data[i][2]+dz);
-        }
-    }
-}
-
-class Face
-{
-    public:
-
-    FaceInfo _faceInfo;
-    Material _material;
-
-    this(FaceInfo fi)
-    {
-        _faceInfo = fi;
-    }
-}
-
-class Tile
-{
-    private:
-        TilePrototype _prototype;
-        Face[] _faces;
-        int _x, _y;
-
-        GObject[] _gobjects;
-    public:
-        this(TilePrototype prototype, int x_, int y_)
-        {
-            _prototype = prototype;
-            _faces.length = prototype.faces.length;
-            for(int i=0; i != prototype.faces.length; ++i)
-            {
-                _faces[i] = new Face(prototype.faces[i].faceinfo);
-            }
-            _x = x_;
-            _y = y_;
-        }
-
-        Material getMaterial(int faceIndex)
-        {
-            if( _faces[faceIndex]._material is null )
-            {
-                _faces[faceIndex]._material = material.getMaterial(_prototype.faces[faceIndex].materialName);
-                _faces[faceIndex]._material.register();
-            }
-            return _faces[faceIndex]._material;
-        }
-
-        void draw()
-        {
-            glEnable(GL_TEXTURE_2D);
-            for(int i=0; i!=_faces.length; ++i)
-            {
-                getMaterial(i).draw();
-                glBegin(GL_QUADS);
-                _faces[i]._faceInfo.draw(x,y,0);
-                glEnd();
-            }
-        }
-
-        void compile()
-        {
-            for(int i=0; i!=_faces.length; ++i)
-            {
-                getMaterial(i).compile();
-                glBegin(GL_QUADS);
-                _faces[i]._faceInfo.draw(x,y,0);
-                glEnd();
-            }
-        }
-
-        int x() { return _x; }
-        int y() { return _y; }
-
-        bool blocking()
-        {
-            if( _prototype.blocking )
-                return true;
-
-            foreach(GObject gobject; _gobjects)
-            {
-                if( gobject.blocking )
-                    return true;
-            }
-            return false;
-        }
-
-        GObject[] getObjects() { return _gobjects; }
-
-        mixin BindClass!("C/TILE");
-}
-
-struct TileFace
-{
-    FaceInfo faceinfo;
-    string materialName;
-}
+public import
+    gobject,
+    tile;
 
 class TilePrototype
 {
@@ -148,27 +29,28 @@ class TilePrototype
     string name;
     bool blocking;
 
-    TileFace[] faces;
+    StaticMesh _mesh;
 
-    this(Dataset data, string name_, sofu.Map info)
+    this(string name_, sofu.Map info)
     {
         name = name_;
+        _mesh = new StaticMesh(name_);
+
         writefln ("Loading Tile Prototype: '%s'",name);
         blocking = info.value("blocking").toInt() != 0;
 
         foreach(sofu.SofuObject element; info.list("faces"))
         {
             writefln ("Face added: %s", element.asList().value(0).toString());
-            TileFace f;
-            f.faceinfo = data._faceInfos[ element.asList().value(0).toString() ];
-            f.materialName = element.asList().value(1).toString();
-            faces ~= f;
+            Face face = Dataset.instance.getFace( element.asList().value(0).toString() );
+            string material = element.asList().value(1).toString();
+            _mesh.addFace( material, face);
         }
     }
 
     Tile create(int x, int y)
     {
-        Tile t = new Tile(this,x,y);
+        Tile t = new Tile(_mesh,x,y);
         return t;
     }
 
@@ -176,16 +58,26 @@ class TilePrototype
     mixin BindMethods!(create);
 }
 
+
 class Dataset
 {
+    static Dataset _instance;
+    this()
+    {
+        _instance = this;
+    }
+
     public:
-        FaceInfo[string] _faceInfos;
+        Face[string] _faces;
 
         TilePrototype[string]   _prototypes;
         GObjectPrototype[string] _objects;
 
-    this()
+    static Dataset instance()
     {
+        if(_instance is null)
+            _instance = new Dataset;
+        return _instance;
     }
 
     void load(string filename)
@@ -203,14 +95,14 @@ class Dataset
         foreach(wchar[] name, sofu.SofuObject element; faces)
         {
             writefln( "-- Loading Face Type: %s", name);
-            _faceInfos[toUTF8(name)] = new FaceInfo(element.asList());
+            _faces[toUTF8(name)] = Face(element.asList());
         }
 
         sofu.Map protoMap = map.map("prototypes");
         foreach(wchar[] name, sofu.SofuObject element; protoMap)
         {
             writefln( "-- Loading Tile Type: %s", name);
-            _prototypes[toUTF8(name)] = new TilePrototype(this, toUTF8(name), element.asMap());
+            _prototypes[toUTF8(name)] = new TilePrototype(toUTF8(name), element.asMap());
         }
 
         sofu.Map entityMap = map.map("objects");
@@ -230,6 +122,8 @@ class Dataset
     {
       return _objects[name];
     }
+
+    Face getFace(string name) { return _faces[name]; }
 
     mixin BindClass!("C/DATASET");
     mixin BindMethods!(load,getTilePrototype,getGObjectPrototype);
--- src/modelmesh.d
+++ src/modelmesh.d
@@ -1,6 +1,11 @@
 module modelmesh;
 
 import texture;
+import material;
+import derelict.opengl.gl;
+import std.stdio;
+
+import sofu = Sofu.Sofu;
 
 // animation state
 struct AnimationState
@@ -20,21 +25,150 @@ struct AnimationState
 
 }
 
-class ModelMesh
+
+class Mesh
+{
+    private:
+        char[] _filename;
+        float  _scale = .05;       // scale value
+
+        float[3]   _pre_translation  = [0, 0, 0];
+        float[3]   _post_translation = [0, 0, 0];
+        float[4][] _rotations;
+
+        float _facing = 0.0;
+
+    public:
+
+    this(char[] filename_)
+    {
+        _filename = filename_;
+    }
+
+    abstract void renderFrame();
+
+    float getScale() { return _scale; }
+    void setScale(float scale) { _scale = scale; }
+
+    void setPreTranslation(in float[3] t) { _pre_translation[] = t[]; }
+    void setPostTranslation(in float[3] t) { _post_translation[] = t[]; }
+    void addRotation(in float[4] r) { _rotations ~= r; }
+    void setFacing(float f) { _facing = f; }
+    void beginDraw(float x, float y, float z)
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glTranslatef(x+_pre_translation[0],y+_pre_translation[1],z+_pre_translation[2]);
+        foreach(float[4] rots; _rotations)
+        {
+            glRotatef(rots[0],rots[1],rots[2],rots[3]);
+        }
+        glRotatef(_facing,0,0,1);
+        glTranslatef(_post_translation[0],_post_translation[1],_post_translation[2]);
+        glScalef(1, 1, 1);
+    }
+    void endDraw()
+    {
+        glPopMatrix();
+    }
+}
+
+struct Face
 {
     public:
-        char[] filename;
-        int    numFrames;         // number of frames
+        float[3][] _vertices;
+        float[2][] _texcoords;
+        float[3][] _normals;
 
-        AnimationState animationState;
-        Texture texture;
-        float   scale = .05;       // scale value
+    static Face opCall()
+    {
+        Face face;
+        return face;
+    }
 
-
-        this(char[] filename_)
+    static Face opCall(sofu.List list)
+    {
+        Face face;
+        foreach(sofu.SofuObject f; list)
         {
-            filename = filename_;
+            face._vertices  ~= [ f.asList().value(0).toFloat(),f.asList().value(1).toFloat(),f.asList().value(2).toFloat() ];
+            face._texcoords ~= [ f.asList().value(3).toFloat(),f.asList().value(4).toFloat() ];
+            face._normals   ~= [ f.asList().value(5).toFloat(),f.asList().value(6).toFloat(),f.asList().value(7).toFloat() ];
         }
+        return face;
+    }
 
-        void renderFrame() { assert(0); }
+    void draw()
+    {
+        for(int i=0; i!= _vertices.length; ++i)
+        {
+            glTexCoord2f(_texcoords[i][0],_texcoords[i][1]);
+            glNormal3f(_normals[i][0],_normals[i][1],_normals[i][2]);
+            glVertex3f(_vertices[i][0],_vertices[i][1],_vertices[i][2]);
+//             writefln ("[%d] ",i,_vertices[i]);
+        }
+    }
+}
+
+class StaticMesh : Mesh
+{
+    private:
+        string[] _material_names;
+        Face[] _faces;
+
+    public:
+
+    this(string filename)
+    {
+        super(filename);
+    }
+
+    void addFace(string material, Face face)
+    {
+        _material_names ~= material;
+        _faces ~= face;
+    }
+
+    void renderFrame()
+    {
+        glEnable(GL_TEXTURE_2D);
+        getMaterial(_material_names[0]).register();
+        getMaterial(_material_names[0]).draw();
+        for(int i=0; i!=_faces.length; ++i)
+        {
+//             glLineWidth(2);
+//             glColor3f(0,1,0);
+//             glBegin(GL_LINE_LOOP);
+            glBegin(GL_QUADS);
+            _faces[i].draw();
+            glEnd();
+        }
+    }
+}
+
+
+class AnimatedMesh : Mesh
+{
+    private:
+        int _currentFrame;
+        int _nextFrame;
+        float _interpol;
+
+    public:
+        int    numFrames;         // number of frames
+        Texture texture;
+
+    this(string filename_)
+    {
+        super(filename_);
+    }
+
+    void setCurrentFrame(int f) { _currentFrame = f; }
+    void setNextFrame(int f) { _nextFrame = f; }
+    void setInterpol(float f) { _interpol = f; }
+
+    int getCurrentFrame() { return _currentFrame; }
+    int getNextFrame() { return _nextFrame; }
+    float getInterpol() { return _interpol; }
+
 }
