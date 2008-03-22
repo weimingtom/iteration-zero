e0d322d380c1848efcf20035baee6c54805dfb4b
klaus.blindert@web.de <klaus.blindert@web.de@7b16e3eb-2848-0410-905a-5b7c5a519ae3>
2008-03-22 09:30:29 +0000

    More char stuff.

--- trunk/src/character.d
+++ trunk/src/character.d
@@ -7,17 +7,10 @@ import dlisp.bind;
 class Character : GObject, ICharacter
 {
     mixin AttributedObject!();
-//     int getArmorEffect();
-//     int getShieldEff();
-
-    // STATE VALUES
-//     bool isShieldOn();
-//     bool isShaded();
-
-    // CAPABILITIES
-//     bool canEquip(IItem item);
 
     mixin BindClass!("C/CHARACTER");
     mixin BindMethods!(getSTR,getDEX,getEND,getINT,getPER);
     mixin BindMethods!(getMaxHP,getHP,getMaxEnergy,getEnergy);
+    mixin BindMethods!(getShieldEffect,getArmorEffect,getShaderEffect);
+    mixin BindMethods!(isShieldActive,isShaderActive);
 }
