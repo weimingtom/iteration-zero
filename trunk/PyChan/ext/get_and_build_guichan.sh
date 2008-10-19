4d9c427f43ae6f8b895f2d449a41af38ca8cab17
klaus.blindert@web.de <klaus.blindert@web.de@7b16e3eb-2848-0410-905a-5b7c5a519ae3>
2008-10-19 15:09:40 +0000

    Bumped working guichan rev. Removed fix.

--- trunk/PyChan/ext/get_and_build_guichan.sh
+++ trunk/PyChan/ext/get_and_build_guichan.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-REV=-r1148
+REV=-r1149
 
 mkdir -p guichan
 svn checkout $REV http://guichan.googlecode.com/svn/trunk/src/ guichan/src
 svn checkout $REV http://guichan.googlecode.com/svn/trunk/include/ guichan/include
 svn checkout $REV http://guichan.googlecode.com/svn/trunk/CMake/ guichan/CMake
-# svn cat $REV http://guichan.googlecode.com/svn/trunk/CMakeLists.txt > guichan/CMakeLists.txt
+svn cat $REV http://guichan.googlecode.com/svn/trunk/CMakeLists.txt > guichan/CMakeLists.txt
 
 cd guichan
 cmake .
