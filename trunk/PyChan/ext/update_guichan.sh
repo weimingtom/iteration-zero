77aef635255c6d654c23f9646d5f9f9deb1a0358
klaus.blindert@web.de <klaus.blindert@web.de@7b16e3eb-2848-0410-905a-5b7c5a519ae3>
2008-11-07 10:16:23 +0000

    new scons targets: build_guichan=yes and install

--- trunk/PyChan/ext/update_guichan.sh
+++ trunk/PyChan/ext/update_guichan.sh
@@ -6,12 +6,9 @@ mkdir -p guichan
 svn checkout $REV http://guichan.googlecode.com/svn/trunk/src/ guichan/src
 svn checkout $REV http://guichan.googlecode.com/svn/trunk/include/ guichan/include
 svn checkout $REV http://guichan.googlecode.com/svn/trunk/CMake/ guichan/CMake
-svn cat $REV http://guichan.googlecode.com/svn/trunk/CMakeLists.txt > guichan/CMakeLists.txt
+svn cat $REV http://guichan.googlecode.com/svn/trunk/CMakeLists.txt > guichan/CMakeLists.in
+sed -e 's/PROJECT(guichan)/PROJECT(guichan_x)/' \
+  < guichan/CMakeLists.in > guichan/CMakeLists.txt
 
-cd guichan
-cmake .
-cmake -DCMAKE_INSTALL_PREFIX:PATH="../../install"
-make
-make install
-cd ..
+
 
